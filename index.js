42ec56bda03af14fbd917666e911029ae3783778
Rajpravinkumar <rajpravinkumaras@gmail.com>
2025-03-11 16:19:53 +0530

    url update

--- index.js
+++ index.js
@@ -16,13 +16,13 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
-/* const corsOptions = {
-  origin: "http://localhost:1573",
+const corsOptions = {
+  origin: "http://localhost:5173",
   credentials: true,
 };
 
-app.use(cors(corsOptions)); */
-app.use(cors())
+app.use(cors(corsOptions));
+//app.use(cors())
 
 const PORT = process.env.PORT || 3000;
 
